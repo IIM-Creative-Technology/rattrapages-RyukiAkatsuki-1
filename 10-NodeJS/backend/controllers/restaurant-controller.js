41ff42df631982902ed93dd85f75b20b3ac5de15
R_K <shawnfroste@outlook.fr>
2023-06-19 15:42:37 +0200

    add updatedRestaurant function

--- 10-NodeJS/backend/controllers/restaurant-controller.js
+++ 10-NodeJS/backend/controllers/restaurant-controller.js
@@ -28,3 +28,22 @@ export const addRestaurant = async (req, res) => {
     }
     return res.status(200).json({newRestaurant});
 }
+
+export const updateRestaurant = async (req, res) => {
+    const {address, zipcode, capacity} = req.body;
+    const restaurantId = req.params.id;
+    let updatedRestaurant;
+    try {
+        updatedRestaurant = await Restaurant.findByIdAndUpdate(restaurantId, {
+            address, 
+            zipcode, 
+            capacity
+        });
+    } catch (err) {
+        return console.log(err);
+    }
+    if(!updatedRestaurant) {
+        return res.status(404).json({message: "Unable to update restaurant"});
+    }
+    res.status(200).json({updatedRestaurant});
+}
