12455b34775cc2dfd900e9a4a9c1a7a94c7069bc
R_K <shawnfroste@outlook.fr>
2023-06-19 19:22:41 +0200

    add more ignoring, another package and link to the database

--- 10-NodeJS/backend/server.js
+++ 10-NodeJS/backend/server.js
@@ -1,10 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
 const port = 3000;
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+.then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+    });
+})
+.catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
+});
+
 app.use(express.json());
-app.listen(port,
-    console.log(`Server is listening on port ${port}`)
-);
