f11ee768180259bf3a5aafa252f4086f12c9d204
R_K <shawnfroste@outlook.fr>
2023-06-20 13:14:57 +0200

    fetch API with loop and simple style

--- 10-NodeJS/backend/server.js
+++ 10-NodeJS/backend/server.js
@@ -6,6 +6,13 @@ import restaurantRouter from './routes/restaurant-route.js';
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI, {
--- 10-NodeJS/frontend/src/js/script.js
+++ 10-NodeJS/frontend/src/js/script.js
@@ -1,15 +1,42 @@
-const restaurantName = document.getElementById('restaurant-name');
-const restaurantAddress = document.getElementById('restaurant-address');
-const restaurantZipCode = document.getElementById('restaurant-zipcode');
-const restaurantCapacity = document.getElementById('restaurant-capacity');
-
+const restaurantContainer = document.getElementById('restaurant-container');
 
 fetch("http://localhost:3000/api/restaurant")
-.then(response => response.json())
-.then(restaurantData => {
-    getData(restaurantData);
-})
+  .then(response => response.json())
+  .then(restaurantData => {
+    console.log(restaurantData);
+    displayData(restaurantData);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
 
-function getData(restaurantData){
-    console.log(restaurantData)
+function displayData(restaurantData) {
+  for (let i = 0; i < restaurantData.restaurants.length; i++) {
+    const restaurant = restaurantData.restaurants[i];
+
+    const restaurantCard = document.createElement('div');
+    restaurantCard.classList.add('restaurantCard');
+
+    const name = document.createElement('h3');
+    name.innerHTML = restaurant.name;
+    restaurantCard.appendChild(name);
+
+    const content = document.createElement('div');
+    content.classList.add('restaurantContent');
+
+    const address = document.createElement('p');
+    address.innerHTML = "Adresse du restaurant: " + restaurant.address;
+    content.appendChild(address);
+
+    const zipcode = document.createElement('p');
+    zipcode.innerHTML = "Code postal du restaurant: " + restaurant.zipcode;
+    content.appendChild(zipcode);
+
+    const capacity = document.createElement('p');
+    capacity.innerHTML = "Capacité du restaurant: " + restaurant.capacity;
+    content.appendChild(capacity);
+
+    restaurantCard.appendChild(content);
+    restaurantContainer.appendChild(restaurantCard);
+  }
 }
